328008f23f9265d43c2346ed66fbba946f7667e4
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-04-14 08:34:48 +0800

    Add Lab2 pt1-cone-area

---
+++ Lab2-cone-area/Lab2-cone-area.ino
@@ -0,0 +1,119 @@
+const int LED = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+
+  float radius = getFloat("Radius of the cone: ");
+  Serial.println(radius);
+  float height = getFloat("Height of cone: ");
+  Serial.println(height);
+
+  float volume = M_PI * sq(radius) * height/3;
+  Serial.print("Volume: ");
+  Serial.println(volume);
+
+  // TODO: TSET TO PAG OKS NA ALIS PRINT
+  if (volume < 100) {
+    Serial.println("LOW");
+    blinkLEDThrice(5000, 5000);
+
+  }
+  else if (100 <= volume <= 500) {
+    Serial.println("MID");
+    blinkLEDThrice(3000, 5000);
+  }
+  else {
+    Serial.println("HIGHEST");
+    blinkLEDThrice(1000, 3000);
+  }
+
+  askAgain();
+}
+
+
+float getFloat(char* message) {
+  while (true) {
+    Serial.print(message);
+    while (!Serial.available()) {
+    }
+    String input = Serial.readStringUntil('\n');
+    input.trim();
+
+    // TODO: CHECK FOR OVERFLOW (OVF)
+
+    // Ensures input is not empty
+    if (input.length() == 0) {
+      Serial.println("ERROR: Cannot be empty.");
+      continue;
+    }
+
+    // Ensures input is not a negative number
+    if (input[0] == '-') {
+      Serial.println("ERROR: Cannot be negative.");
+      continue;
+    }
+    
+    // Ensures first letter is number
+    if (!isDigit(input[0])) {
+      Serial.println("ERROR: First character must be a digit.");
+      continue;
+    }
+
+    // Ensures input has no non digits and has at most 1 decimal point
+    bool hasNonDigit = false;
+    int decimalCount = 0;
+    for (int i = 1; i < input.length(); i++) {
+      if (input[i] == '.') {
+        decimalCount++;
+      }
+      else if (!isDigit(input[i])) {
+        hasNonDigit = true;
+      }
+    }
+    if (hasNonDigit) {
+      Serial.println("ERROR: Must NOT contain non digits.");
+      continue;
+    }
+    if (decimalCount > 1) {
+      Serial.println("ERROR: Cannot have more than (one) 1 decimal point.");
+      continue;
+    }
+
+    float validFloat = input.toFloat();
+    return validFloat;
+  }
+}
+
+void askAgain() {
+  while (true) {
+    Serial.println("press 0 to try again; press 1 to exit: ");
+    while (!Serial.available()) {
+    }
+    String answer = Serial.readStringUntil('\n');
+    answer.trim();
+    if (answer.equals("0")) {
+      break;
+    }
+    else if (answer.equals("1")) {
+      Serial.println("Goodbye!");
+      while (true) {
+      }
+    }
+  }
+
+}
+
+
+void blinkLEDThrice(int onDuration, int offDuration) {
+  for (int i = 0; i < 3; i++) {
+  digitalWrite(LED, HIGH);
+  delay(onDuration);
+  digitalWrite(LED, LOW);
+  delay(offDuration);
+  }
+}

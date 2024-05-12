cf325d7e57e475dc5d3c8302974e5fe6f8ea5deb
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-05-12 13:17:05 +0800

    Add comments, improve edge case handling

--- lab3-different-switches/lab3-different-switches.ino
+++ lab3-different-switches/lab3-different-switches.ino
@@ -1,45 +1,22 @@
+// Buttons
 const int NUM_OF_BTNS = 4;
-const byte BTN1 = 2;
-const byte BTN2 = 3;
-const byte BTN3 = 4;
-const byte BTN4 = 5;
 const byte BTNS[NUM_OF_BTNS] = {
-  BTN1, 
-  BTN2,
-  BTN3, 
-  BTN4,
+  2, 3, 4, 5,
 };
-
-const int NUM_OF_LEDS = 8;
-const int NUM_OF_LEDS_HALF = NUM_OF_LEDS / 2;
-const byte LED1 = 6;
-const byte LED2 = 7;
-const byte LED3 = 8;
-const byte LED4 = 9;
-const byte LED5 = 10;
-const byte LED6 = 11;
-const byte LED7 = 12;
-const byte LED8 = 13;
-const byte LEDS[NUM_OF_LEDS] = {
-  LED1, 
-  LED2, 
-  LED3, 
-  LED4, 
-  LED5, 
-  LED6, 
-  LED7, 
-  LED8,
-};
-
 byte isBtnsPressed[NUM_OF_BTNS] = {
-  LOW,
-  LOW,
-  LOW,
-  LOW,
+  LOW, LOW, LOW, LOW,
 };
-
+// Used to handle edge cases
 int lastBtnPressed = -1;
 
+// LEDs
+const int NUM_OF_LEDS = 8;
+const int NUM_OF_LEDS_HALF = NUM_OF_LEDS / 2;
+const byte LEDS[NUM_OF_LEDS] = {
+  6, 7, 8, 9, 10, 11, 12, 13,
+};
+
+// Delays for majority of the LED patterns
 int durationInMS = 1000;
 int delayInMS = durationInMS / NUM_OF_LEDS;
 
@@ -47,41 +24,42 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
+  // Cofigure modes for btn and LED pins
   for (int i = 0; i < NUM_OF_BTNS; i++) {
     pinMode(BTNS[i], INPUT_PULLUP);
   }
-
   for (int i = 0; i < NUM_OF_LEDS; i++) {
     pinMode(LEDS[i], OUTPUT);
   }
 }
 
+
 void loop() {
   // put your main code here, to run repeatedly:
+  // Check button presses
   int i = 0;
   while (i < NUM_OF_BTNS) {
     isBtnsPressed[i] = digitalRead(BTNS[i]);
-    Serial.print(digitalRead(BTNS[i]));
-    Serial.print(digitalRead(BTN1));
     i++;
   }
 
   handleEdgeCases();
   clearLEDs();
-  
+
+  // Light LED patterns based on button pressed
   if (isBtnsPressed[0]) {
     blinkTraverse();
     lastBtnPressed = 0;
+    handleEdgeCases();
   }
   if (isBtnsPressed[1]) {
     blinkInward(false);
     lastBtnPressed = 1;
-
+    handleEdgeCases();
   }
   if (isBtnsPressed[2]) {
     blinkInward(true);
     lastBtnPressed = 2;
-
   }
   if (isBtnsPressed[3]) {
     blinkGapped();
@@ -90,41 +68,20 @@ void loop() {
 }
 
 
-void handleEdgeCases() {
-  bool is123Pressed = false;
-  for (int i = 0; i < NUM_OF_BTNS - 1; i++) {
-    is123Pressed |= isBtnsPressed[i];
-  }
-  if (!is123Pressed) {
-    if (lastBtnPressed == 0) {
-      digitalWrite(LEDS[0], HIGH);
-      delay(delayInMS);
-      digitalWrite(LEDS[0], LOW);
-    }
-    else if (lastBtnPressed == 1) {
-      digitalWrite(LEDS[0], HIGH);
-      digitalWrite(LEDS[NUM_OF_LEDS - 1], HIGH);
-      delay(delayInMS);
-      digitalWrite(LEDS[0], LOW);
-      digitalWrite(LEDS[NUM_OF_LEDS - 1], LOW);
-    }
-    lastBtnPressed = -1;
-  }
-}
-
 void clearLEDs() {
   for (int i = 0; i < NUM_OF_LEDS; i++) {
     digitalWrite(LEDS[i], LOW);
   }
 }
 
+
 void blinkTraverse() {
+  /* Blink from left to right, then right to left */
   clearLEDs();
   for (int i = 0; i < NUM_OF_LEDS; i++) {
     digitalWrite(LEDS[i], HIGH);
     delay(delayInMS);
     digitalWrite(LEDS[i], LOW);
-
   }
 
   for (int i = NUM_OF_LEDS - 2; i > 0; i--) {
@@ -134,19 +91,24 @@ void blinkTraverse() {
   }
 }
 
+
 void blinkInward(bool keep) {
+  /* Blinks inward from left & right to center, 
+  then outward from center to left & right 
+
+  keep = keeps light for passed LEDs */
   int i = 0;
   int j = NUM_OF_LEDS - 1;
-
   clearLEDs();
+
   while (i < j) {
     digitalWrite(LEDS[i], HIGH);
     digitalWrite(LEDS[j], HIGH);
     delay(delayInMS);
 
     if (!keep) {
-    digitalWrite(LEDS[i], LOW);
-    digitalWrite(LEDS[j], LOW);
+      digitalWrite(LEDS[i], LOW);
+      digitalWrite(LEDS[j], LOW);
     }
     i++;
     j--;
@@ -154,10 +116,9 @@ void blinkInward(bool keep) {
 
   if (keep) {
     clearLEDs();
-  }
-  else {
-  i--;
-  j++;
+  } else {
+    i--;
+    j++;
   }
 
   i--;
@@ -168,8 +129,8 @@ void blinkInward(bool keep) {
     delay(delayInMS);
 
     if (!keep) {
-    digitalWrite(LEDS[i], LOW);
-    digitalWrite(LEDS[j], LOW);
+      digitalWrite(LEDS[i], LOW);
+      digitalWrite(LEDS[j], LOW);
     }
     i--;
     j++;
@@ -182,12 +143,14 @@ void blinkInward(bool keep) {
   }
 }
 
+
 void blinkGapped() {
+  /* Blink two LEDs with a gap, from right to left */
   int delayInMS = 500;
   int i = NUM_OF_LEDS - 3;
   int j = NUM_OF_LEDS - 1;
-  
   clearLEDs();
+
   while (i > -1) {
     digitalWrite(LEDS[i], HIGH);
     digitalWrite(LEDS[j], HIGH);
@@ -196,5 +159,24 @@ void blinkGapped() {
     digitalWrite(LEDS[j], LOW);
     i--;
     j--;
+  }
+}
+
+
+void handleEdgeCases() {
+  /* Makes LED patterns for Switch1 and Switch 2 smoother */
+  if ((!isBtnsPressed[0] || isBtnsPressed[3]) && lastBtnPressed == 0) {
+    digitalWrite(LEDS[0], HIGH);
+    delay(delayInMS);
+    digitalWrite(LEDS[0], LOW);
+    lastBtnPressed = -1;
+  } 
+  else if ((!isBtnsPressed[1] || isBtnsPressed[0] || isBtnsPressed[3]) && lastBtnPressed == 1) {
+    digitalWrite(LEDS[0], HIGH);
+    digitalWrite(LEDS[NUM_OF_LEDS - 1], HIGH);
+    delay(delayInMS);
+    digitalWrite(LEDS[0], LOW);
+    digitalWrite(LEDS[NUM_OF_LEDS - 1], LOW);
+    lastBtnPressed = -1;
   }
 }

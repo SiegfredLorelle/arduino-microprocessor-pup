8c7276b8591665796c0017a7acc3bd8c9ef25658
Gaeus Caskie <95890979+OvertheSkyy@users.noreply.github.com>
2024-06-07 05:14:04 +0800

    Add note frequencies

--- lab7-prob4-password-driven-piezo-buzzer/lab7-prob4-password-driven-piezo-buzzer.ino
+++ lab7-prob4-password-driven-piezo-buzzer/lab7-prob4-password-driven-piezo-buzzer.ino
@@ -18,15 +18,55 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {9, 8, 7, 6};
 byte colPins[COLS] = {5, 4, 3, 2};
 
-// Define the LED pins
-const int ledPins[] = {10, 11, 12, 13};
-const int numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
-
 // Create an object of Keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 // Global password for verification
 const String globalPassword = "ABCD1234#";
+
+// Define the LED pins
+const int ledPins[] = {10, 11, 12, 13};
+const int numLeds = sizeof(ledPins) / sizeof(ledPins[0]);
+
+// Define note frequencies
+const int REST = 0;
+const int NOTE_C3 = 131;
+const int NOTE_CS3 = 139;
+const int NOTE_D3 = 147;
+const int NOTE_DS3 = 156;
+const int NOTE_E3 = 165;
+const int NOTE_F3 = 175;
+const int NOTE_FS3 = 185;
+const int NOTE_G3 = 196;
+const int NOTE_GS3 = 208;
+const int NOTE_A3 = 220;
+const int NOTE_AS3 = 233;
+const int NOTE_B3 = 247;
+const int NOTE_C4 = 262;
+const int NOTE_CS4 = 277;
+const int NOTE_D4 = 294;
+const int NOTE_DS4 = 311;
+const int NOTE_E4 = 330;
+const int NOTE_F4 = 349;
+const int NOTE_FS4 = 370;
+const int NOTE_G4 = 392;
+const int NOTE_GS4 = 415;
+const int NOTE_A4 = 440;
+const int NOTE_AS4 = 466;
+const int NOTE_B4 = 494;
+const int NOTE_C5 = 523;
+const int NOTE_CS5 = 554;
+const int NOTE_D5 = 587;
+const int NOTE_DS5 = 622;
+const int NOTE_E5 = 659;
+const int NOTE_F5 = 698;
+const int NOTE_FS5 = 740;
+const int NOTE_G5 = 784;
+const int NOTE_GS5 = 831;
+const int NOTE_A5 = 880;
+const int NOTE_AS5 = 932;
+const int NOTE_B5 = 988;
+const int NOTE_C6 = 1047;
 
 void setup() {
   Serial.begin(9600);

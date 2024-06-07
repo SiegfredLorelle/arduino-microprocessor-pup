80f8580f38d9ddacadac659e4e2b69484603e0e0
SiegfredLorelle <siegfredlorellecmina@iskolarngbayan.pup.edu.ph>
2024-06-07 15:16:35 +0800

    Bugfix: incorrect operation, remove trailing and ending 0s

--- lab7-prob3-calculator/lab7-prob3-calculator.ino
+++ lab7-prob3-calculator/lab7-prob3-calculator.ino
@@ -60,7 +60,7 @@ void menu() {
     lcd.setCursor(0, 1);
     lcd.print(lines[row2]);
 
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 15; i++) {
       if (keypad.getKey()) {
         lcd.clear();
         return;
@@ -79,6 +79,7 @@ void menu() {
 
 
 char currOperator = ' ';
+char pendingOperator = ' ';
 int currRow = 0;
 void calculator() {
     String strNums[2] = {
@@ -92,17 +93,12 @@ void calculator() {
     Serial.println(strNums[currRow]);
     Serial.println(currOperator);
     
-    String result = processOperation(currOperator, strNums[0], strNums[1]);
+    String result = processOperation(strNums[0], strNums[1]);
     strNums[0] = result;
     strNums[1] = "";
     displayDigit(0, strNums[0]);
     displayDigit(1, strNums[1]);
     currRow = 1;
-
-
-    // Get 2nd num
-    
-
   }
 }
 
@@ -124,10 +120,15 @@ String getDigit() {
     }
     else {
       if (key != '=') {
-        currOperator = key;
-        displayOperator(currOperator);
+        displayOperator(key);
+        pendingOperator = key;
+        if (currOperator == ' ') currOperator = key;
       }
-      if (strDigit.length() > 0) return strDigit;
+      if (strDigit.length() > 0) {
+
+        return trimDigit(strDigit);
+      }
+      else currOperator = key;
     }
   }
 }
@@ -144,10 +145,9 @@ void displayOperator(char currOperator) {
     lcd.print(currOperator);
 }
 
-String processOperation(char currOperator, String strNum1, String strNum2) {
-      // displayOperator(currOperator);
-    // }
+String processOperation(String strNum1, String strNum2) {
     if (strNum1.length() == 0 || strNum2.length() == 0) return strNum1;
+
     float num1 = strNum1.toFloat(); 
     float num2 = strNum2.toFloat(); 
     float res;
@@ -164,13 +164,48 @@ String processOperation(char currOperator, String strNum1, String strNum2) {
     else if (currOperator == '/') {
       res = num1 / num2;
     }
+
+    Serial.println(res);
+    currOperator = pendingOperator;
+    // serial.println(currOperator);
     
     if (isnan(res) || isinf(res)) res = 0;
-    return String(res);
+    Serial.print("RESULT: ");
+    return trimDigit(String(res));
+}
+
+String trimDigit(String digit) {
+  String newDigitStart = "";
+  String newDigitEnd = "";
+  int start = 0;
+  int end = digit.length() - 1;
+
+  while (start < digit.length() - 1) {
+    if (digit[start] == '0') start++;
+    else break;
+  }
+
+  if (digit[digit.length() - 3] == '.') {
+    while (end >= 0) {
+      if (digit[end] == '0') end--;
+      else break;
+    }
+  }
+
+
+  String newDigit = digit.substring(start, end + 1);
+  if (newDigit[0] == '.') {
+    newDigit = "0" + newDigit;
+  }
+  if (newDigit[newDigit.length() - 1] == '.') {
+    newDigit.remove(newDigit.length() - 1);
+  }
+
+  return newDigit;
+
 }
 
 // Handle edge cases
 // Clear all
-// Do not display redundancy
 // Remove negative if res = 0
 // Limit result
